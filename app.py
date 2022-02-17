80d0a6dd39d6538dbd60126ae786eb454474ddb2
jbeedle19 <josh.beedle@gmail.com>
2022-02-17 13:39:03 -0500

    added login/logout routes

--- app.py
+++ app.py
@@ -31,3 +31,47 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+# Login Route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """ Log user in """
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST
+    if request.method == "POST":
+
+        # Ensure username and password was submitted
+        if not request.form.get("username") or not request.form.get("password"):
+            return apology("must provide username/password", 403)
+        
+        # Query DB for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect to dashboard/watchlist
+        return redirect("/dashboard")
+
+    # User reached route via GET
+    else:
+        return render_template("login.html")
+
+
+# Logout Route
+@app.route("/logout")
+def logout():
+    """ Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to homepage
+    return redirect("/")
